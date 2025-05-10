1ab8b058c414ce14b422bad7da56a69d7a9c94b9
adityachaubeycode <aditya09chaubey@gmail.com>
2025-05-10 13:58:01 +0530

    adde tkinter

--- tictacgame.py
+++ tictacgame.py
@@ -1,1 +1,39 @@
-print("Welcome to Tic Tac game ")
+import tkinter as tk
+from tkinter import messagebox
+
+def check_win():
+    for combo in [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8,], [2,4,6]]:
+        if buttons[combo[0]]["text"] == buttons[combo[1]]["text"] == buttons[combo[2]]["text"] !="":
+            buttons[combo[0]].config(bg ="green")
+            buttons[combo[1]].config(bg ="green")
+            buttons[combo[2]].config(bg ="green")
+            winner = True
+            messagebox.showinfo("tic-tac-toe", f"Player {buttons[combo[0]]['text']} wins!")
+            root.quit()
+
+def buttons_click(index):
+    if buttons[index]["text"] == "" and not winner:
+        buttons[index]["text"] = current_player
+        check_win()
+        if not winner:
+            toggle_player()
+
+    def toggle_player():
+        global current_player
+        current_player = "X" if current_player == "O"  else "O"
+        label.config(text=f"Player {current_player}'s turn")
+
+root = tk.Tk()
+root.title("tic-tac-toe")
+
+buttons = [tk.Button(root, text="", font=("normal", 25), width=6, height=2, command=lambda i=i: buttons_click(i)) for i in range (9)]
+
+for i, button in enumerate(buttons):
+    button.grid(row=i //3, column=i % 3)
+
+current_player = "X"
+winner = False
+label = tk.Label(root, text=f"player{current_player}'s turn", font=("normal", 16)) 
+label.grid(row=3, column=0, columnspan=3)
+
+root.mainloop()
